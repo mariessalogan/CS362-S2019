400dd027a9c14b41ba84378af578409bcd244e90
mariessalogan <38187604+mariessalogan@users.noreply.github.com>
2019-05-19 19:38:05 -0700

    Added larger coverage radius for dominion.c

--- dominion/randomtestadventurer.c
+++ dominion/randomtestadventurer.c
@@ -13,7 +13,7 @@ int main()
 {
 	srand(time(NULL));
 
-	int seed = 150;
+	int seed = 100;
 	int i;
 	for(i=0; i <= seed; i++)
 	{
@@ -25,35 +25,42 @@ int main()
 		int handPos = rand() % 5;
 		struct gameState G;
 		struct gameState *state = newGame();
-		int k[10] = {adventurer, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy, council_room};
-		kingdomCards(k[0], k[1], k[2], k[3], k[4], k[5], k[6], k[7], k[8], k[9]);
-		initializeGame(numPlayers, k, seed, &G);
+		int k[12] = {adventurer, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy, council_room, gardens, great_hall};
+		int index = 0;
+		int array[10];
+		for(index = 0; index < 10 ; index++)
+		{
+			array[index] = rand() %13;
+		}
+		int *cards = kingdomCards(k[0], k[1], k[2], k[3], k[4], k[5], k[6], k[7], k[8], k[9]);
+		initializeGame(numPlayers, cards, seed, &G);
 		//this will be to pick a random plater to add different types of cards to their hand.
-		int i;
+		int l;
 		//This is to put random cards in that random person's hands
 		int j;
 		int m;
+		int n = rand() % 2;
 		for(m = 0; m<10; m++){
 			//there are 27 numbers in the card enumerator
 			j = rand() % 28;
 			int cost = getCost(j);
-			i = rand() % 5;
+			l = rand() % 5;
 			G.supplyCount[m] = i;
 
 		}
-		cardEffect(j, 1, 2, 3, &G, handPos, k);
+		cardEffect(j, n, n, n, &G, handPos, k);
 		//These are needed to add coverage.
 		numHandCards(&G);
-		scoreFor(i, &G);
-		playCard(handPos, i, j, m, &G);
-		fullDeckCount(i, j, &G);
+		scoreFor(l, &G);
+		playCard(handPos, l, j, m, &G);
+		fullDeckCount(l, j, &G);
 		supplyCount(j, &G);
 		handCard(handPos, &G);
 		endTurn(&G);
 		buyCard(handPos, &G);
 		isGameOver(&G);
 		//test adventurer function
-		adventurerFunction(i, &G, temphand, j, m, drawntreasure);
+		adventurerFunction(l, &G, temphand, j, m, drawntreasure);
 		//test on it's own and make sure drawn treasure is at 2
 		printf("--------- Testing Function on it's own ----------\n");
 		printf("-------- Expected outcome: drawntreasure = 2 ---------\n");
